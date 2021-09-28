dd635b1007d08e65b1f4e9b9c0b4380f0629e8de
manjiriphatak <85757379+manjiriphatak@users.noreply.github.com>
2021-09-28 18:57:23 -0400

    Updated the cityDisplay

--- src/CityDisplay.js
+++ src/CityDisplay.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./CityDisplay.css";
+import Search from "./Search";
 
 export default function CityDisplay(props) {
-  return <div className="CityDisplay">{props.name}</div>;
+  return <div className="CityDisplay">Toronto</div>;
 }
